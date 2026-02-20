0086bd4357151ce18d7daf469c6c7b17a5e8b74d
franchem <franchem@educ.college-montreal.qc.ca>
2026-02-20 10:45:34 -0500

    acdancements

--- attack_animation.py
+++ attack_animation.py
@@ -1,28 +1,28 @@
 import arcade
-
+from pathlib import Path
 
 class Animated_Sprite(arcade.Sprite):
-    def __init__(self, path, rows, columns, sizeX, sizeY):
+    def __init__(self, path, mirrored=False):
 
         super().__init__()
 
-        # code pour transformer une spritesheet en spritelist
-        self.texture_list = []
-        sheet = arcade.load_spritesheet(f"{path}.png")
-        texture_list = sheet.get_texture_grid(
-            size=(sizeX, sizeY),
-            columns=columns,
-            count=rows*columns
-        )
-        for a in texture_list:
-            self.texture_list.append(a)
+        dir_path = Path(path)
+        self.textures = []
+        for file in dir_path.iterdir():
+            self.textures.append(arcade.load_texture(path+"/"+file.name))
+        if mirrored:
+            mirrored_list = self.textures[::-1]
+            mirrored_list.pop(0)
+            mirrored_list.pop(-1)
+            for texture in mirrored_list:
+                self.textures.append(texture)
 
         self.cur_texture_index = 0
-        self.textures = self.texture_list
         self.set_texture(self.cur_texture_index)
+
         self.current_delay = 0
         self.animation_delay = 0.1
-        self.scale = 4
+        self.scale = 0.5
         self.playing = False
 
     def update(self, delta_time):
@@ -32,5 +32,5 @@ class Animated_Sprite(arcade.Sprite):
                 self.current_delay = 0
                 self.set_texture(self.cur_texture_index)
                 self.cur_texture_index += 1
-                if self.cur_texture_index >= len(self.textures)-1:
+                if self.cur_texture_index == len(self.textures):
                     self.cur_texture_index = 0
--- main.py
+++ main.py
@@ -13,37 +13,60 @@ class GameView(ac.Window):
     def __init__(self):
         super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
         self.state = game_state.State.NOT_STARTED
-        self.rock = attack_animation.Animated_Sprite("animations/test", 1, 9, 18, 18)
+
+        self.human = ac.Sprite("assets/human.png")
+        self.computer = ac.Sprite("assets/compy.png")
+
+        self.dynamics = {}
+        self.dynamics["rock"] = attack_animation.Animated_Sprite("assets/animations/rock", mirrored=True)
 
     def setup(self):
-        self.rock.position = (500, 500)
+        self.dynamics["rock"].position = (100, 150)
+
+        self.human.position = (250, 300)
+        self.human.scale = 2
+        self.computer.position = (800, 300)
+        self.computer.scale = 2
 
     def on_key_press(self, symbol: int, modifiers: int) -> EVENT_HANDLE_STATE:
         if self.state == game_state.State.GAME_OVER:
             pass
+        self.state = game_state.State.ROUND_ACTIVE
 
 
     def draw_static(self):
         ac.draw_text("Roche, Papier, Ciseaux", 150, 800, (50, 150, 150), 60)
+        # carres blancs
+        ac.draw.draw_lrbt_rectangle_outline(50, 150, 100, 200, (200, 200, 200), 10)
+        ac.draw.draw_lrbt_rectangle_outline(200, 300, 100, 200, (200, 200, 200), 10)
+        ac.draw.draw_lrbt_rectangle_outline(350, 450, 100, 200, (200, 200, 200), 10)
+        # carre rouge
+        ac.draw.draw_lrbt_rectangle_outline(750, 850, 100, 200, (250, 0, 0), 10)
+
+        # faces
+        ac.draw_sprite(self.human, pixelated=True)
+        ac.draw_sprite(self.computer, pixelated=True)
 
     def on_draw(self):
         self.clear()
         self.draw_static()
 
-        ac.draw_sprite(self.rock, pixelated=True)
+        ac.draw_sprite(self.dynamics["rock"], pixelated=True)
 
         cursorbox.draw_information()
 
     def on_update(self, delta_time: float) -> bool | None:
-        self.rock.update(delta_time)
+        for key in self.dynamics:
+            self.dynamics[key].update(delta_time)
 
     def on_mouse_motion(self, x: float, y: float, shit, shit2):
         cursorbox.mouseX = x
         cursorbox.mouseY = y
-        if self.rock.collides_with_point((x, y)):
-            self.rock.playing = True
-        else:
-            self.rock.playing = False
+        for key in self.dynamics:
+            if self.dynamics[key].collides_with_point((x, y)):
+                self.dynamics[key].playing = True
+            else:
+                self.dynamics[key].playing = False
 
 def main():
     window = GameView()
