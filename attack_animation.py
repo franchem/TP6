327567f653d35879bce8ad5729ba5c6e6c324e94
franchem <franchem@educ.college-montreal.qc.ca>
2026-03-23 16:19:54 -0400

    trying to add the computer sprite

--- attack_animation.py
+++ attack_animation.py
@@ -2,7 +2,7 @@ import arcade
 from pathlib import Path
 
 class Animated_Sprite(arcade.Sprite):
-    def __init__(self, path, mirrored=False, scale=0.5):
+    def __init__(self, path, mirrored=False, scale=1):
 
         super().__init__()
 
@@ -25,6 +25,38 @@ class Animated_Sprite(arcade.Sprite):
         self.scale = scale
         self.playing = False
 
+    def update(self, delta_time):
+        if self.playing:
+            self.current_delay += delta_time
+            if self.current_delay > self.animation_delay:
+                self.current_delay = 0
+                self.set_texture(self.cur_texture_index)
+                self.cur_texture_index += 1
+                if self.cur_texture_index == len(self.textures):
+                    self.cur_texture_index = 0
+
+class Multi_Animation_sprite(arcade.Sprite):
+    def __init__(self, lst, mirrored=False, scale=1):
+        super().__init__()
+        self.anim_list = []
+        for path in lst:
+            dir_path = Path(path)
+            self.anim_list.append([])
+            for file in dir_path.iterdir():
+                self.anim_list[-1].append(arcade.load_texture(path + "/" + file.name))
+            if mirrored:
+                mirrored_list = self.anim_list[-1][::-1]
+                mirrored_list.pop(0)
+                mirrored_list.pop(-1)
+                for texture in mirrored_list:
+                    self.anim_list[-1].append(texture)
+        self.textures = self.anim_list[0]
+        self.cur_texture_index = 0
+        self.set_texture(self.cur_texture_index)
+        self.current_delay = 0
+        self.animation_delay = 0.1
+        self.scale = scale
+        self.playing = False
     def update(self, delta_time):
         if self.playing:
             self.current_delay += delta_time
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import arcade as ac
 from pyglet.event import EVENT_HANDLE_STATE
-
-import game_state, attack_animation, cursorbox
+from enum import Enum
+import game_state, attack_animation, cursorbox, random
 WINDOW_WIDTH, WINDOW_HEIGHT = 1000, 1000
 WINDOW_TITLE = "Dwayne Johnson, Fichiers Epstein, Couple Lesbienne"
+
+class Choices(Enum):
+    ROCK = 0
+    PAPER = 2
+    SCISSORS = 3
 
 class GameView(ac.Window):
     """
@@ -18,34 +23,45 @@ class GameView(ac.Window):
         self.computer = ac.Sprite("assets/compy.png")
 
         self.dynamics = {}
-        self.dynamics["rock"] = attack_animation.Animated_Sprite("assets/animations/rock", mirrored=True)
-        self.dynamics["paper"] = attack_animation.Animated_Sprite("assets/animations/test", mirrored=True, scale=5)
-        self.dynamics["scissors"] = attack_animation.Animated_Sprite("assets/animations/test", mirrored=True, scale=5)
+        self.dynamics["rock"] = attack_animation.Animated_Sprite("assets/animations/rock", mirrored=True, scale=5)
+        self.dynamics["paper"] = attack_animation.Animated_Sprite("assets/animations/paper", mirrored=True, scale=5)
+        self.dynamics["scissors"] = attack_animation.Animated_Sprite("assets/animations/scissors", mirrored=True, scale=5)
+        self.dynamics["robot"] = attack_animation.Multi_Animation_sprite(["assets/animations/test",
+                                                                          "assets/animations/rock",
+                                                                          "assets/animations/paper",
+                                                                          "assets/animations/scissors"], mirrored=True, scale=5)
+
+        self.human_wins = 0
+        self.robot_wins = 0
+        self.robot_choice = 0
+        self.clash_result = "rien"
 
     def setup(self):
         self.dynamics["rock"].position = (100, 150)
-        self.dynamics["paper"].position = (250, 150)
-        self.dynamics["scissors"].position = (400, 150)
+        self.dynamics["paper"].position = (280, 150)
+        self.dynamics["scissors"].position = (460, 150)
 
-        self.human.position = (250, 300)
-        self.human.scale = 2
-        self.computer.position = (800, 300)
-        self.computer.scale = 2
+        self.human.position = (280, 350)
+        self.human.scale = 2.5
+        self.computer.position = (800, 350)
+        self.computer.scale = 2.5
 
     def on_key_press(self, symbol: int, modifiers: int) -> EVENT_HANDLE_STATE:
         if self.state == game_state.State.GAME_OVER:
-            pass
+            self.human_wins = 0
+            self.robot_wins = 0
         self.state = game_state.State.ROUND_ACTIVE
+        self.clash_result = random.choice(("draw", "com win", "hum win"))
 
 
     def draw_static(self):
         ac.draw_text("Roche, Papier, Ciseaux", 150, 800, (50, 150, 150), 60)
         # carres blancs
-        ac.draw.draw_lrbt_rectangle_outline(50., 150, 100, 200, (200, 200, 200), 10)
-        ac.draw.draw_lrbt_rectangle_outline(200, 300, 100, 200, (200, 200, 200), 10)
-        ac.draw.draw_lrbt_rectangle_outline(350, 450, 100, 200, (200, 200, 200), 10)
+        ac.draw.draw_lrbt_rectangle_outline(20., 180, 70, 230, (100, 100, 100), 10)
+        ac.draw.draw_lrbt_rectangle_outline(200, 360, 70, 230, (100, 100, 100), 10)
+        ac.draw.draw_lrbt_rectangle_outline(380, 540, 70, 230, (100, 100, 100), 10)
         # carre rouge
-        ac.draw.draw_lrbt_rectangle_outline(750, 850, 100, 200, (250, 0, 0), 10)
+        ac.draw.draw_lrbt_rectangle_outline(720, 880, 70, 230, (250, 0, 0), 10)
 
         # faces
         ac.draw_sprite(self.human, pixelated=True)
@@ -58,6 +74,11 @@ class GameView(ac.Window):
         ac.draw_sprite(self.dynamics["rock"], pixelated=True)
         ac.draw_sprite(self.dynamics["paper"], pixelated=True)
         ac.draw_sprite(self.dynamics["scissors"], pixelated=True)
+
+        if self.state == game_state.State.NOT_STARTED:
+            ac.draw_text("Pesez sur espace pour débuter", 100, 700, (255, 255, 255), 50)
+        elif self.state == game_state.State.ROUND_ACTIVE:
+            ac.draw_text("Appuyez sur un des trois icones", 100, 700, (255, 255, 255), 50)
 
         cursorbox.draw_information()
 
@@ -74,6 +95,24 @@ class GameView(ac.Window):
             else:
                 self.dynamics[key].playing = False
 
+    def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
+        for key in self.dynamics:
+            if self.dynamics[key].collides_with_point((x, y)):
+                self.state = game_state.State.ROUND_DONE
+                if key == "rock":
+                    choice = Choices.ROCK
+                if key == "paper":
+                    choice = Choices.PAPER
+                else:
+                    choice = Choices.SCISSORS
+
+                if self.clash_result == "draw":
+                    self.robot_choice = choice
+                elif self.clash_result == "com win":
+                    self.robot_choice = (choice+1)%3
+                else:
+                    self.robot_choice = (choice-1)%3
+                self.dynamics["robot"].textures = self.dynamics["robot"].anim_list[self.robot_choice+1]
 def main():
     window = GameView()
     window.setup()
